c5e95cd7a779e33645a43aa387bc3a12caa28d73
Gustavo Gorgônio <ggorgon03@gmail.com>
2022-08-02 04:36:07 -0300

    adding errors messages to create method

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,6 +11,13 @@ class ArticlesController < ApplicationController
     end
 
     def create
-        render plain: params[:article]
+        @article = Article.new(params.require(:article).permit(:title, :description))
+        if @article.save
+            flash[:notice] = 'Article was created successfully'
+            redirect_to article_path(@article)
+        else
+            flash[:errors] = @article.errors.full_messages
+            render new_article_path
+        end
     end
 end
