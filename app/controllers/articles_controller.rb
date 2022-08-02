dc0e3a9ef8adc1bae53b783e0edc4596a7cfaa56
Gustavo Gorgônio <ggorgon03@gmail.com>
2022-08-02 02:48:08 -0300

    adding new and create methods for Articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,11 @@ class ArticlesController < ApplicationController
     def show
         @article = Article.find(params[:id])
     end
+
+    def new
+    end
+
+    def create
+        render plain: params[:article]
+    end
 end
