5c0cd14f3b76f39ee972210e97f42c295d49817a
Gustavo Gorgônio <ggorgon03@gmail.com>
2022-08-05 23:39:56 -0300

    styling listing articles page

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,13 @@
 module ArticlesHelper
+    def created_time_ago_in_words(article)
+        "created #{time_ago_in_words(article.created_at)} ago"
+    end
+
+    def edited_time_ago_in_words(article)
+        "edited #{time_ago_in_words(article.updated_at)} ago"
+    end
+
+    def created_edited_time_ago_in_words(article)
+        "#{created_time_ago_in_words(article)}, #{edited_time_ago_in_words(article)}"
+    end
 end
