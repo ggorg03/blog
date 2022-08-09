ede32ce7b8289eb1469525abec45d5719d5a9b86
Gustavo Gorgônio <ggorgon03@gmail.com>
2022-08-09 18:49:28 -0300

    removing field_with_erros of inputs helpers

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,8 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# remove field_with_erros of inputs
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+  html_tag.html_safe
+end
