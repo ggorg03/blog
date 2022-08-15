e7a9c918ee61997f70abb9cea21ac6f1cacd6522
Gustavo Gorgônio <ggorgon03@gmail.com>
2022-08-15 18:41:00 -0300

    add show user action

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,4 +2,12 @@ module UsersHelper
     def form_action_name(user)
         user.new_record? ? 'Sign up!' : 'Update'
     end
+
+    def gravatar_image(user, options={size: 80})
+        email_address = user.email.downcase
+        hash = Digest::MD5.hexdigest(email_address)
+        image_src = "https://www.gravatar.com/avatar/#{hash}?s=#{options[:size]}"
+
+        image_tag image_src, alt: @user.username , class: "rounded-circle mb-5 shadow mx-auto d-block"
+    end
 end
