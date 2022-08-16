0bce8204b4911a1e33d8fd77d437ae7eb8a5d23c
Gustavo Gorgônio <ggorgon03@gmail.com>
2022-08-16 01:35:16 -0300

    add users index

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -8,6 +8,10 @@ module UsersHelper
         hash = Digest::MD5.hexdigest(email_address)
         image_src = "https://www.gravatar.com/avatar/#{hash}?s=#{options[:size]}"
 
-        image_tag image_src, alt: @user.username , class: "rounded-circle mb-5 shadow mx-auto d-block"
+        image_tag image_src, alt: user.username , class: "rounded-circle mb-5 shadow mx-auto d-block"
+    end
+
+    def joined_time_ago_in_words(user)
+        "Joined #{time_ago_in_words(user.created_at)} ago" 
     end
 end
