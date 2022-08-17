808b5ecac7c12ecaa9de4dd8ef2de9a9841f8251
Gustavo Gorgônio <ggorgon03@gmail.com>
2022-08-17 00:38:00 -0300

    add create action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,15 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:notice] = 'Logged successfully'
+      redirect_to user
+    else
+      flash.now[:errors] = ['There is some error on email or password']
+      render 'new'
+    end
   end
 
   def destroy
