fa10caedb682bcd741978c00c86cf0eea9cd975e
Gustavo Gorgônio <ggorgon03@gmail.com>
2022-08-17 00:47:24 -0300

    add logout action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
-      flash[:notice] = 'Logged successfully'
+      flash[:notice] = 'Logged in successfully'
       redirect_to user
     else
       flash.now[:errors] = ['There is some error on email or password']
@@ -15,5 +15,8 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    session[:user_id] = nil
+    flash[:notice] = 'Logged out successfully'
+    redirect_to root_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,5 @@ Rails.application.routes.draw do
 
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
-  get 'sessions/destroy'
+  delete '/logout', to: 'sessions#destroy'
 end
