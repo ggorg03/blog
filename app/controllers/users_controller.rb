e7d47b47812ac36d7e4f7ffeb6fce8b2e10a2ef7
Gustavo Gorgônio <ggorgon03@gmail.com>
2022-08-18 02:45:36 -0300

    add auto log in when create account

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,19 @@ class UsersController < ApplicationController
     def edit
     end
 
+
+    def create
+        @user = User.new(user_params)        
+        if @user.save
+            session[:user_id] = @user.id
+            flash[:notice] =  "Welcome to Alpha Blog, #{@user.username}"
+            redirect_to users_path(@user)
+        else
+            flash[:errors] = @user.errors.full_messages
+            redirect_to signup_path
+       end
+    end
+
     def update
         if @user.update(user_params)
             flash[:notice] = "User was successfully updated"
@@ -26,17 +39,6 @@ class UsersController < ApplicationController
             flash[:errors] = @user.errors.full_messages
             redirect_to edit_user_path(@user)
         end
-    end
-
-    def create
-        @user = User.new(user_params)        
-        if @user.save
-            flash[:notice] =  "Welcome to Alpha Blog, #{@user.username}"
-            redirect_to users_path(@user)
-        else
-            flash[:errors] = @user.errors.full_messages
-            redirect_to signup_path
-       end
     end
 
     def destroy
