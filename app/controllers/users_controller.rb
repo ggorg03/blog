d504301374faa26684339738d3332357c0455277
Gustavo Gorgônio <ggorgon03@gmail.com>
2022-08-17 23:57:50 -0300

    restrict actions at user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
     before_action :set_user, only: [:show, :edit, :update]
+    before_action :require_user, only: [:edit, :update]
+    before_action :require_same_user, only: [:edit, :update]
 
     def show
         @articles = @user.articles.paginate(page: params[:page], per_page: 5).order('updated_at DESC')
@@ -45,5 +47,12 @@ class UsersController < ApplicationController
 
     def user_params
         params.require(:user).permit(:username, :email, :password)
+    end
+
+    def require_same_user
+        if @user != current_user
+            flash[:alert] = 'You just can edit your Profile'
+            redirect_to @user
+        end
     end
 end
