ee439e5aba8f7ec150efb80ecf8c23a42501d23c
Gustavo Gorgônio <ggorgon03@gmail.com>
2022-08-20 01:35:56 -0300

    Add admon column to Users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_15_155307) do
+ActiveRecord::Schema.define(version: 2022_08_20_031336) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -25,7 +25,8 @@ ActiveRecord::Schema.define(version: 2022_08_15_155307) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "password_digest"
+    t.string "password_digest", default: "123"
+    t.boolean "admin", default: false
   end
 
 end
