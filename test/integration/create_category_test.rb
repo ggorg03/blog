7c47d997fb7f210d5c9552b3de4d1a258527611f
Gustavo Gorgônio <ggorgon03@gmail.com>
2022-08-25 23:46:02 -0300

    add test if invalid category can be created

--- test/integration/create_category_test.rb
+++ test/integration/create_category_test.rb
@@ -1,11 +1,11 @@
 require 'test_helper'
 
 class CreateCategoryTest < ActionDispatch::IntegrationTest
-  test "can create a category" do
+  test "get new create and show category" do
     get new_category_path
     assert_response :success
 
-    assert_difference('Category.count', 1) do
+    assert_difference 'Category.count', 1 do
       post categories_path, params: {category: {name: "Sports"}}
     end
     assert_response :redirect
@@ -14,5 +14,18 @@ class CreateCategoryTest < ActionDispatch::IntegrationTest
     assert_response :success
     
     assert_match 'Category: Sports', @response.body
+  end
+
+  test "get new but not create and show invalid category" do
+    get new_category_path
+    assert_response :success
+
+    assert_no_difference 'Category.count' do
+      post categories_path, params: { category: { name: nil } }
+      post categories_path, params: { category: { name: "" } }
+      post categories_path, params: { category: { name: " " } }
+      post categories_path, params: { category: { name: "a" * 2 } }
+      post categories_path, params: { category: { name: "a" * 26 } }
+    end
   end
 end
