7451436cd3bb42872085199f79ea9f23112818fe
Gustavo Gorgônio <ggorgon03@gmail.com>
2022-08-25 22:30:26 -0300

    add create action to category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -7,8 +7,27 @@ class CategoriesController < ApplicationController
     end
 
     def new
+        @category = Category.new
+    end
+
+    def create
+        @category = Category.new(category_params)
+
+        if @category.save
+            flash['notice'] = 'Category was created successfully!'
+            redirect_to @category
+        else
+            flash['alert'] = @category.errors.full_messages
+            redirect_to new_category_path
+        end
     end
 
     def edit
+    end
+
+    private 
+
+    def category_params
+        params.require(:category).permit(:name)
     end
 end
